19272493791759f5e82f1bafe2fe0fa950c99fe6
Tamim <tamim193002014@gmail.com>
2025-03-20 13:01:53 +0300

    js  updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -221,3 +221,7 @@ function changeLanguage() {
   }
 }
 
+if (window.location.pathname.endsWith(".html")) {
+  let newPath = window.location.pathname.replace(".html", "");
+  window.history.replaceState(null, "", newPath);
+}
